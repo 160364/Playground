03b1d2e89e4814686a67d5ee74ef6edce1606fe4
160364 <52152073+160364@users.noreply.github.com>
2019-07-25 21:20:55 +0530

    Test code pushed to repository.

---
+++ Greatest of two number/Main.java
@@ -0,0 +1,10 @@
+#include<stdio.h>
+ void main()
+{
+  int num1=6,num2=8;
+  scanf("%d%d",&num1,&num2);
+  if(num1>num2)
+    printf("num1 is the greatest");
+  else
+    printf("num2 is the greatest");
+}
